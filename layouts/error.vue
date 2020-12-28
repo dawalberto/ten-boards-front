25414fc635bf90ee2ae0ea8d3c29c72551eb8c17
Alberto García <albertoinformatica97@gmail.com>
2020-12-28 18:24:35 +0100

    error page

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-center text-center">
+    <div v-if="error.statusCode === 404" class="p-80">
+      <h1
+        class="text-9xl text-gradient bg-gradient-to-r from-blue-500 to-purple-600 font-light"
+      >
+        {{ error.statusCode }}
+      </h1>
+      <p v-if="error.message" class="mt-4">{{ error.message }}</p>
+    </div>
+    <h1 v-else class="p-80">An error occurred</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style></style>
