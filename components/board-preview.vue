25214aad2dd051034142f7245e3938862d52d5cd
Alberto García <albertoinformatica97@gmail.com>
2021-01-05 02:02:46 +0100

    init boards preview ✅

---
+++ components/board-preview.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="group px-6 py-4 flex flex-col sm:flex-row justify-between items-center cursor-pointer bg-gradient-to-r hover:from-blue-500 hover:to-purple-600"
+  >
+    <div class="flex-1 self-start md:self-center">
+      <p class="font-bold group-hover:text-white">{{ title }}</p>
+      <p :title="$t('updated-at')" class="font-medium group-hover:text-white">
+        <svg
+          class="w-6 h-6 inline"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1"
+            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        {{ updatedAt }}
+      </p>
+    </div>
+    <p class="flex-1 mt-2 md:mt-0 group-hover:text-white">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    updatedAt: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/boards.vue
+++ pages/boards.vue
@@ -1,9 +1,54 @@
 <template>
   <div>
-    <h1>totalBoards: {{ totalBoards }}</h1>
-    <h1>finishedBoards: {{ finishedBoards }}</h1>
-    <h1>unfinishedBoards: {{ unfinishedBoards }}</h1>
-    <pre>{{ boards }}</pre>
+    <div class="flex w-full flex-row-reverse">
+      <button class="btn-primary fl-upper">
+        {{ $t('new-board') }}
+      </button>
+    </div>
+    <div class="flex flex-col w-full mt-2 rounded-md shadow-md">
+      <div
+        class="rounded-t-md p-4 flex justify-start items-center bg-gradient-to-r from-blue-500 to-purple-600"
+      >
+        <!-- Heroicons: clipboard-list -->
+        <svg
+          class="w-8 h-8 mr-4 text-white"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1"
+            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+          ></path>
+        </svg>
+        <p
+          class="mr-2 cursor-pointer capitalize"
+          :class="seeingUnfinishedBoards ? 'text-white' : 'text-gray-300'"
+          @click="showUnfinishedBoards"
+        >
+          <span class="text-xl">{{ totalFinishedBoards }}</span>
+          {{ $t('open') }}
+        </p>
+        <p
+          class="mr-2 cursor-pointer capitalize"
+          :class="!seeingUnfinishedBoards ? 'text-white' : 'text-gray-300'"
+          @click="showFinishedBoards"
+        >
+          <span class="text-xl">{{ totalUnfinishedBoards }}</span>
+          {{ $t('finished') }}
+        </p>
+      </div>
+      <board-preview
+        v-for="board of boards"
+        :key="board._id"
+        :title="board.title"
+        :updated-at="board.updatedAt"
+        :description="board.description"
+      ></board-preview>
+    </div>
   </div>
 </template>
 
@@ -11,10 +56,12 @@
 export default {
   data() {
     return {
-      boards: null,
-      totalBoards: 0,
-      finishedBoards: 0,
-      unfinishedBoards: 0,
+      boards: [],
+      finishedBoards: [],
+      unfinishedBoards: [],
+      totalFinishedBoards: 0,
+      totalUnfinishedBoards: 0,
+      seeingUnfinishedBoards: true,
     }
   },
   mounted() {
@@ -26,8 +73,6 @@ export default {
         .get('/api/boards')
         .then((response) => {
           const responseData = response.data
-          this.getBoards(responseData)
-          this.getTotalBoards(responseData)
           this.getFinishedBoards(responseData)
           this.getUnfinishedBoards(responseData)
         })
@@ -35,19 +80,22 @@ export default {
           console.log(error)
         })
     },
-    getBoards(data) {
-      this.boards = data.boards
-    },
-    getTotalBoards(data) {
-      this.totalBoards = data.total
-    },
     getFinishedBoards(data) {
-      this.finishedBoards = data.boards.filter((board) => board.finished).length
+      this.finishedBoards = data.boards.filter((board) => board.finished)
+      this.totalFinishedBoards = this.finishedBoards.length
     },
     getUnfinishedBoards(data) {
-      this.unfinishedBoards = data.boards.filter(
-        (board) => !board.finished
-      ).length
+      this.unfinishedBoards = data.boards.filter((board) => !board.finished)
+      this.totalUnfinishedBoards = this.unfinishedBoards.length
+      this.showUnfinishedBoards()
+    },
+    showUnfinishedBoards() {
+      this.boards = this.unfinishedBoards
+      this.seeingUnfinishedBoards = true
+    },
+    showFinishedBoards() {
+      this.boards = this.finishedBoards
+      this.seeingUnfinishedBoards = false
     },
   },
 }
