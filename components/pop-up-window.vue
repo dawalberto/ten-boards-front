e41024fe77a79cdfacbe9aaf2d1d911f76f72d63
Alberto García <albertoinformatica97@gmail.com>
2021-01-14 22:48:00 +0100

    pop-up-window component

---
+++ components/pop-up-window.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    id="background"
+    class="z-50 fixed top-0 left-0 w-screen h-screen p-4 md:p-0"
+  >
+    <div
+      class="h-full w-full md:h-2/5 md:w-3/4 lg:w-2/4 xl:w-2/5 md:fixed md:top-2/4 md:left-2/4 md:transform md:-translate-x-2/4 md:-translate-y-2/4 rounded-md shadow-xl"
+    >
+      <div class="flex h-12 items-center bg-primary rounded-t-md">
+        <h1 class="ml-2 text-white text-lg capitalize">{{ $t(title) }}</h1>
+      </div>
+      <div class="p-4">
+        <slot></slot>
+      </div>
+      <div
+        class="flex content-end p-4 absolute bottom-0 left-0 flex-row-reverse w-full"
+      >
+        <button class="btn ml-2 capitalize bg-green-400">
+          {{ $t('accept') }}
+        </button>
+        <button class="btn capitalize bg-red-400">{{ $t('cancel') }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style>
+#background {
+  backdrop-filter: blur(0.1rem);
+}
+</style>
