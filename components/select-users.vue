466dc63fef56f2e23a48585647b70fcce449c20b
Alberto García <albertoinformatica97@gmail.com>
2021-01-14 22:59:33 +0100

    select-users component

---
+++ components/select-users.vue
@@ -0,0 +1,76 @@
+<template>
+  <Autocomplete
+    id="test"
+    :search="search"
+    :auto-select="true"
+    placeholder="Search for a user"
+    aria-label="Search for a user"
+    :get-result-value="getResultValue"
+    @submit="onSelectUser"
+  >
+    <template #result="{ result, props }">
+      <li v-bind="props">
+        <div class="flex items-end">
+          <UserAvatar :svg="result.avatar" :size="7" class="mr-2" />
+          <span>{{ result.userName }}</span>
+        </div>
+        <span class="mt-1">{{ result.name }}</span>
+      </li>
+    </template>
+  </Autocomplete>
+</template>
+
+<script>
+import Autocomplete from '@trevoreyre/autocomplete-vue'
+import UserAvatar from '~/components/user-avatar.vue'
+
+export default {
+  components: { Autocomplete, UserAvatar },
+  props: {
+    users: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    search(input) {
+      if (input.length < 1) {
+        return []
+      }
+
+      input = input.toLowerCase()
+      return this.users.filter((user) => {
+        return (
+          user.name.toLowerCase().includes(input) ||
+          user.userName.toLowerCase().startsWith(input) ||
+          user.email.toLowerCase().startsWith(input)
+        )
+      })
+    },
+    onSelectUser(user) {
+      alert(user.userName)
+    },
+    getResultValue() {
+      return ''
+    },
+  },
+}
+</script>
+
+<style>
+.autocomplete {
+  @apply border border-indigo-600 rounded-md px-2 inline-block w-full;
+}
+
+.autocomplete-input {
+  @apply outline-none my-2;
+}
+
+.autocomplete-result-list {
+  @apply m-0 p-0 -ml-2 rounded-md shadow-lg;
+}
+
+.autocomplete-result {
+  @apply p-2 rounded-md cursor-pointer hover:shadow-md;
+}
+</style>
